82f83638a96a36e673abbd9fd2ffb6e07277326f
Alimuddin Hasan <alimudhasan@gmail.com>
2022-07-31 23:54:56 +0700

    feat: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,22 @@
-import Head from 'next/head'
-
-import styles from '@/pages/index.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className='h-screen flex flex-col'>
       <Head>
         <title>Next.js Base App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js Base App!</a>
-        </h1>
-
+      <main className='flex-1 flex flex-col justify-center items-center'>
+        <h1 className='font-bold text-5xl'>Welcome to Next.js Base App!</h1>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://alimuddinhasan.com"
-          target="_blank"
-        >
-          Created by{' '}
-          <span className={styles.logo}>
-            Alimuddin Hasan
-          </span>
+      <footer className='h-24 w-full flex justify-center items-center'>
+        <a href='https://alimuddinhasan.com' target='_blank'>
+          Created by <span>Alimuddin Hasan</span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
