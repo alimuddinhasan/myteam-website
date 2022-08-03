516e468bf77c00b5ed642d0ee95f2b09c05c5090
Alimuddin Hasan <alimudhasan@gmail.com>
2022-08-03 23:17:48 +0700

    feat: add number trivia with react form hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,36 @@
 import Head from "next/head";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "store";
+import { joiResolver } from "@hookform/resolvers/joi";
+import Joi from "joi";
 
 import numberTriviaUsecase from "domain/usecase/numberTrivia.usecase";
 import { addBy, reduceBy } from "store/counter/counterSlice";
+import { useForm } from "react-hook-form";
+
+const schema = Joi.object({
+  number: Joi.number().required(),
+});
+
+interface IFormData {
+  number: number;
+}
 
 export default function Home() {
   const counter = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
-  useEffect(() => {
-    numberTriviaUsecase.getNumberTrivia(10).then((data) => {
-      console.log(data);
-    });
-  }, []);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm<IFormData>({
+    resolver: joiResolver(schema),
+  });
+
+  const submitHandler = handleSubmit(async (data) => {
+    const trivia = await numberTriviaUsecase.getNumberTrivia(data.number);
+    console.log(trivia);
+  });
   return (
     <div className='h-screen flex flex-col'>
       <Head>
@@ -39,6 +56,26 @@ export default function Home() {
             Reduce by 5
           </button>
         </div>
+        <h1 className='font-bold text-4xl'>Number Trivia App!</h1>
+        <form onSubmit={submitHandler} className='flex gap-3 items-start'>
+          <div>
+            <input
+              {...register("number")}
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              placeholder='Number'
+            />
+            {errors.number && (
+              <p className='text-red-600'>{errors.number.message}</p>
+            )}
+          </div>
+
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            type='submit'
+          >
+            Submit
+          </button>
+        </form>
       </main>
 
       <footer className='h-24 w-full flex justify-center items-center'>
