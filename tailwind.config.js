0385eb4fe4afd2c7b12cbb0f550a750cabff218f
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-03 23:57:28 +0700

    style: setup tailwind style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'midnight-green': '#014E56',
+        'light-coral': '#F67E7E',
+        'white': '#FFFFFF',
+        'rapture-blue': '#79C8C7',
+        'police-blue': '#2C6269',
+        'deep-jungle-green': '#004047',
+        'sacramento-state-green': '#012F34',
+        'dark-green': '#002529',
+      }
+    },
   },
   plugins: [],
 }
