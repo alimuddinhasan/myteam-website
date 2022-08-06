beb23479b670667d8c221f01f164c66826545584
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-06 22:46:43 +0700

    feat: add screen files

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <Fragment>
       <Head>
-        <title>Frontend Mentor | myteam website challenge - Homepage</title>
+        <title>Frontend Mentor | myteam website challenge - About</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <h1>Homepage</h1>
+        <h1>About</h1>
       </main>
     </Fragment>
   );
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <Fragment>
       <Head>
-        <title>Frontend Mentor | myteam website challenge - Homepage</title>
+        <title>Frontend Mentor | myteam website challenge - Contact</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <h1>Homepage</h1>
+        <h1>Contact</h1>
       </main>
     </Fragment>
   );
