8a05aa99850b3babff056f4914815d4b6c263e4d
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-08 16:40:17 +0700

    feat(header): handle hidden or show

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,14 +3,15 @@ import Button from "../Button";
 
 export default function Header() {
   return (
-    <header className='flex justify-center bg-midnight-green py-10'>
+    <header className='flex justify-center bg-midnight-green py-10 pl-5 pr-3'>
       <div className='grow flex justify-between items-center max-w-5xl'>
         <div className='flex items-center'>
           <img src='assets/logo.svg' className='mr-5' alt='myteam-logo' />
-          <Button label='home' isFlat />
-          <Button label='about' isFlat />
+          <Button className='hidden' label='home' isFlat />
+          <Button className='hidden' label='about' isFlat />
         </div>
-        <Button label='contact us' />
+        <Button className='sm:hidden' icon='assets/icon-hamburger.svg' isFlat />
+        <Button className='hidden sm:inline-block' label='contact us' />
       </div>
     </header>
   );
