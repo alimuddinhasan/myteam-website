0427ca7fd042befab74a3358e42329275c886473
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-16 08:45:09 +0700

    feat(container): add className in container

--- components/Container/__test__/index.test.tsx
+++ components/Container/__test__/index.test.tsx
@@ -2,9 +2,15 @@ import { render } from "@testing-library/react";
 import Container from "..";
 
 describe("Container", () => {
-  it("should render Container component", () => {
+  it("should render default Container component", () => {
     const container = render(<Container>children</Container>);
 
     expect(container.getByText("children")).toBeInTheDocument();
+  });
+
+  it("should render Container component with class", () => {
+    const container = render(<Container className='test'>children</Container>);
+
+    expect(container.getByText("children")).toHaveClass("test");
   });
 });
--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -2,8 +2,11 @@ import React, { ReactNode } from "react";
 
 interface IContainerProps {
   children?: ReactNode | undefined;
+  className?: string | undefined;
 }
 
-export default function Container({ children }: IContainerProps) {
-  return <div className="max-w-6xl w-screen">{children}</div>;
+export default function Container({ children, className }: IContainerProps) {
+  return (
+    <div className={`${className || ""} max-w-6xl w-screen`}>{children}</div>
+  );
 }
