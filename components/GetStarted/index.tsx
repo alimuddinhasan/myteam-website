c8f936a7198739ed80964c1be2c83fa8947ce8e3
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-16 08:46:15 +0700

    feat(get-started): add container to get started

--- components/GetStarted/index.tsx
+++ components/GetStarted/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import Button, { ButtonColor } from "../Button";
+import Container from "../Container";
 
 export default function GetStarted() {
   return (
     <section
-      className='bg-light-coral py-20 bg-[url("/assets/bg-pattern-home-6-about-5.svg")] bg-no-repeat bg-left-bottom'
+      className='bg-light-coral bg-[url("/assets/bg-pattern-home-6-about-5.svg")] bg-no-repeat bg-left-bottom flex justify-center'
       role='get-started'
     >
-      <div className='flex flex-col items-center max-w-5xl gap-5'>
-        <h1 className='font-bold text-2xl text-dark-green text-center'>
-          Ready to get started?
-        </h1>
-        <Button className='py-2' label='contact us' color={ButtonColor.dark} />
-      </div>
+      <Container className="py-20">
+        <div className='flex flex-col items-center gap-5'>
+          <h1 className='font-bold text-2xl text-dark-green text-center'>
+            Ready to get started?
+          </h1>
+          <Button
+            className='py-2'
+            label='contact us'
+            color={ButtonColor.dark}
+          />
+        </div>
+      </Container>
     </section>
   );
 }
