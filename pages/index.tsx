d180a76b512e1dfccd438c4f24a92c1f11970d75
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-16 10:04:40 +0700

    feat(home): add responsive on review section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Review from "@/components/Review";
 import Head from "next/head";
 import { Fragment } from "react";
+import { Container } from "../components";
 
 export default function Home() {
   return (
@@ -25,26 +26,36 @@ export default function Home() {
           </h1>
         </div>
       </section>
-      <section className='bg-deep-jungle-green py-36 px-5 bg-[url("/assets/bg-pattern-home-4-about-3.svg"),_url("/assets/bg-pattern-home-5.svg")] bg-no-repeat bg-[position:left_top,_right_bottom]'>
-        <div className='flex flex-col justify-center max-w-5xl gap-8'>
-          <h1 className='font-bold text-2xl text-center text-white'>
-            Delivering real results for top companies. Some of our{" "}
-            <span className='text-rapture-blue'>success stories.</span>
-          </h1>
-          <Review
-            review='We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!'
-            name='Aiysha Reese'
-            jobTitle='Founder of Manage'
-            avatar='assets/avatar-kady.jpg'
-          />
+      <section className='bg-deep-jungle-green py-36 px-5 bg-[url("/assets/bg-pattern-home-4-about-3.svg"),_url("/assets/bg-pattern-home-5.svg")] bg-no-repeat bg-[position:left_top,_right_bottom] flex justify-center'>
+        <Container>
+          <div className='flex flex-col items-center gap-8'>
+            <h1 className='font-bold text-2xl lg:text-5xl text-center text-white'>
+              Delivering real results for top companies. Some of our{" "}
+              <span className='text-rapture-blue'>success stories.</span>
+            </h1>
+            <div className='flex flex-col lg:flex-row items-center gap-8'>
+              <Review
+                review='We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!'
+                name='Aiysha Reese'
+                jobTitle='Founder of Manage'
+                avatar='assets/avatar-kady.jpg'
+              />
 
-          <Review
-            review='We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!'
-            name='Aiysha Reese'
-            jobTitle='Founder of Manage'
-            avatar='assets/avatar-kady.jpg'
-          />
-        </div>
+              <Review
+                review='We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!'
+                name='Aiysha Reese'
+                jobTitle='Founder of Manage'
+                avatar='assets/avatar-kady.jpg'
+              />
+              <Review
+                review='We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!'
+                name='Aiysha Reese'
+                jobTitle='Founder of Manage'
+                avatar='assets/avatar-kady.jpg'
+              />
+            </div>
+          </div>
+        </Container>
       </section>
       <GetStarted />
       <Footer />
