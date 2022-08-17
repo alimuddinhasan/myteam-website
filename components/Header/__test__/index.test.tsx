079cf71779ee869623b4f087c7cbdc1bb91f75ee
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-17 17:51:22 +0700

    feat(header): add responsive

--- components/Header/__test__/index.test.tsx
+++ components/Header/__test__/index.test.tsx
@@ -7,10 +7,13 @@ describe("Header", () => {
     const header = render(<Header />);
 
     expect(header.getByAltText("myteam-logo")).toBeInTheDocument();
+    expect(header.getByAltText("bg-pattern-about-2-contact-1")).toBeInTheDocument();
+    expect(header.getByAltText("bg-pattern-home-2")).toBeInTheDocument();
     expect(header.getByText("home")).toBeInTheDocument();
     expect(header.getByText("about")).toBeInTheDocument();
     expect(header.getByText("contact us")).toBeInTheDocument();
     expect(header.getByTestId("title")).toBeInTheDocument();
     expect(header.getByTestId("description")).toBeInTheDocument();
+    expect(header.getByTestId("header-h-line")).toBeInTheDocument();
   });
 });
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,12 +5,17 @@ import Container from "../Container";
 export default function Header() {
   return (
     <header
-      className='flex justify-center bg-midnight-green pt-10 px-5'
+      className='flex justify-center bg-midnight-green pt-10 px-5 relative'
       role='header'
     >
-      <Container>
-        <div className="flex flex-col">
-          <div className='grow flex justify-between items-center'>
+      <img
+        className='hidden lg:block max-w-sm absolute left-0 top-1/2 -translate-y-1/2 -translate-x-1/2'
+        src='/assets/bg-pattern-about-2-contact-1.svg'
+        alt='bg-pattern-about-2-contact-1'
+      />
+      <Container className="z-10">
+        <div className='flex flex-col items-center'>
+          <div className='flex justify-between items-center self-stretch'>
             <div className='flex items-center gap-5'>
               <img src='assets/logo.svg' className='mr-5' alt='myteam-logo' />
               <Button className='hidden sm:inline-block' label='home' isFlat />
@@ -23,23 +28,36 @@ export default function Header() {
             />
             <Button className='hidden sm:inline-block' label='contact us' />
           </div>
-          <div className='flex flex-col justify-center py-20'>
+          <div className='flex flex-col lg:flex-row items-center lg:items-stretch py-20 max-w-md lg:max-w-none gap-5'>
             <h1
-              className='text-white text-6xl font-bold text-center mb-5'
+              className='text-white text-6xl sm:text-7xl font-bold text-center lg:text-left lg:basis-1/2'
               data-testid='title'
             >
-              Find the best <span className='text-light-coral'>talent</span>
+              Find the <br />
+              best <span className='text-light-coral'>talent</span>
             </h1>
-            <p
-              className='font-semibold text-base text-center text-white'
-              data-testid='description'
-            >
-              Finding the right people and building high performing teams can be
-              hard. Most companies aren’t tapping into the abundance of global
-              talent. We’re about to change that.
-            </p>
+            <div className='flex flex-col justify-between lg:basis-1/2'>
+              <hr
+                className='hidden lg:block w-12 border-2 border-light-coral'
+                data-testid='header-h-line'
+              />
+              <p
+                className='font-semibold text-base lg:text-sm text-center lg:text-left text-white'
+                data-testid='description'
+              >
+                Finding the right people and building high performing teams can
+                be hard. Most companies aren’t tapping into the abundance of
+                global talent. We’re about to change that.
+              </p>
+            </div>
           </div>
-          <img src='/assets/bg-pattern-home-2.svg' alt='bg-pattern-home-2' />
+          <div className='flex justify-center lg:justify-end self-stretch'>
+            <img
+              className='max-w-sm'
+              src='/assets/bg-pattern-home-2.svg'
+              alt='bg-pattern-home-2'
+            />
+          </div>
         </div>
       </Container>
     </header>
