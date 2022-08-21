eea48be352442b61a259974f63a7ce4bb465584b
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-21 13:13:51 +0700

    feat(director): toggle quote

--- components/Director/__test__/index.test.tsx
+++ components/Director/__test__/index.test.tsx
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import Director from "..";
+import userEvent from "@testing-library/user-event";
 
 describe("Director", () => {
   it("should render Director component", () => {
@@ -19,5 +20,30 @@ describe("Director", () => {
     const directorAvatar = director.getByAltText("director-avatar");
     expect(directorAvatar).toBeInTheDocument();
     expect(directorAvatar).toHaveAttribute("src", "avatar");
+
+    expect(director.queryByText("quote")).toBeNull();
+  });
+
+  it("should show quote", async () => {
+    const director = render(
+      <Director
+        avatar='avatar'
+        name='name'
+        positon='position'
+        quote='quote'
+        socials={{ linkedin: "linkedin", twitter: "twitter" }}
+      />
+    );
+
+    expect(director.getByText("position")).toBeInTheDocument();
+    expect(director.getByAltText("director-avatar")).toBeInTheDocument();
+    expect(director.queryByText(/quote/)).toBeNull();
+
+    const buttonPlus = director.getByTestId("button-plus");
+
+    await userEvent.click(buttonPlus);
+    expect(director.getByText(/quote/)).toBeInTheDocument();
+    expect(director.queryByText("position")).toBeNull();
+    expect(director.queryByAltText("director-avatar")).toBeNull();
   });
 });
--- components/Director/index.tsx
+++ components/Director/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import Button from "../Button";
 
 interface IDirectorProps {
@@ -19,27 +19,49 @@ export default function Director({
   quote,
   socials,
 }: IDirectorProps) {
+  const [showQuote, setShowQuote] = useState(false);
+
+  const generateButtonClases = () => {
+    if (showQuote) {
+      return "rotate-45 bg-rapture-blue";
+    }
+    return "bg-light-coral";
+  };
+
   return (
-    <div className='bg-sacramento-state-green relative mb-10'>
-      <div className='flex flex-col items-center py-5 gap-5 '>
-        <img
-          className='w-24 rounded-full border-2'
-          src={avatar}
-          alt='director-avatar'
-        />
-        <div className='mb-10'>
-          <h2 className='text-rapture-blue text-sm font-bold text-center'>
-            {name}
-          </h2>
+    <div className='bg-sacramento-state-green relative mb-10 px-10'>
+      <div className='flex flex-col items-center py-10 mb-5'>
+        {!showQuote && (
+          <img
+            className='w-24 rounded-full border-2 mb-5'
+            src={avatar}
+            alt='director-avatar'
+          />
+        )}
+        <h2 className='text-rapture-blue text-sm font-bold text-center'>
+          {name}
+        </h2>
+        {!showQuote && (
           <p className='italic font-medium text-white text-center'>{positon}</p>
-        </div>
-        <Button
-          className='bg-light-coral w-14 h-14 flex justify-center items-center rounded-full absolute bottom-0 translate-y-1/2'
-          isFlat
-          icon='/assets/icon-cross.svg'
-          dataTestid='button-plus'
-        />
+        )}
+        {showQuote && (
+          <Fragment>
+            <p className='font-medium text-white text-center my-5'>"{quote}"</p>
+            <div className='flex justify-center gap-5'>
+              <Button icon='assets/icon-twitter.svg' isFlat />
+              <Button icon='assets/icon-linkedin.svg' isFlat />
+            </div>
+          </Fragment>
+        )}
       </div>
+
+      <Button
+        className={`${generateButtonClases()} w-14 h-14 flex justify-center items-center rounded-full absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2`}
+        isFlat
+        icon='/assets/icon-cross.svg'
+        dataTestid='button-plus'
+        onClick={() => setShowQuote(!showQuote)}
+      />
     </div>
   );
 }
