b7d3fa75282117fc4ebe38d581a6667316fb4502
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-23 23:20:08 +0700

    feat(contact): add page title

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,16 +1,20 @@
-import Head from "next/head";
-import { Fragment } from "react";
+import BaseScreen from "@/components/BaseScreen";
+import Container from "@/components/Container";
 
-export default function Home() {
+export default function Contact() {
   return (
-    <Fragment>
-      <Head>
-        <title>Frontend Mentor | myteam website challenge - Contact</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <main>
-        <h1>Contact</h1>
-      </main>
-    </Fragment>
+    <BaseScreen
+      pageTitle='Frontend Mentor | myteam website challenge - Contact'
+      showGetStarted={false}
+    >
+      <section role='contact ' className='bg-midnight-green relative'>
+        <Container>
+          <div className='flex flex-col items-center gap-5 px-5 py-24'>
+            <h1 className='text-4xl font-bold text-white'>Contact</h1>
+            <h2 className='text-3xl font-bold text-light-coral'>Ask us about</h2>
+          </div>
+        </Container>
+      </section>
+    </BaseScreen>
   );
 }
