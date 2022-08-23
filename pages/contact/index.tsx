df73c109976a5b168da6c16b96358109283ba374
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-08-23 23:44:44 +0700

    feat(contact): implement contact item

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,4 +1,5 @@
 import BaseScreen from "@/components/BaseScreen";
+import ContactItem from "@/components/ContactItem";
 import Container from "@/components/Container";
 
 export default function Contact() {
@@ -7,11 +8,27 @@ export default function Contact() {
       pageTitle='Frontend Mentor | myteam website challenge - Contact'
       showGetStarted={false}
     >
-      <section role='contact ' className='bg-midnight-green relative'>
+      <section role='contact' className='bg-midnight-green flex justify-center'>
         <Container>
           <div className='flex flex-col items-center gap-5 px-5 py-24'>
             <h1 className='text-4xl font-bold text-white'>Contact</h1>
-            <h2 className='text-3xl font-bold text-light-coral'>Ask us about</h2>
+            <h2 className='text-3xl font-bold text-light-coral'>
+              Ask us about
+            </h2>
+            <div className='flex flex-col gap-5'>
+              <ContactItem
+                icon='/assets/icon-person.svg'
+                label='The quality of our talent network'
+              />
+              <ContactItem
+                icon='/assets/icon-cog.svg'
+                label='Usage & implementation of our software'
+              />
+              <ContactItem
+                icon='/assets/icon-chart.svg'
+                label='How we help drive innovation'
+              />
+            </div>
           </div>
         </Container>
       </section>
