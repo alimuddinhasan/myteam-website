356101fe9cf36f346604e9c858a8907c16101a1e
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-09-01 05:45:45 +0700

    feat(contact): add responsive

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -17,43 +17,45 @@ export default function Contact() {
         <img
           src="/assets/bg-pattern-contact-2.svg"
           alt="bg-top-left"
-          className="absolute right-0 bottom-0 translate-x-1/2 translate-y-1/2"
+          className="absolute right-0 bottom-0 translate-x-1/2 md:translate-y-0 translate-y-1/2"
         />
         <Container className="py-24">
-          <div className="flex flex-col items-center gap-5 px-5 mb-10">
-            <h1 className="text-4xl font-bold text-white">Contact</h1>
-            <h2 className="text-3xl font-bold text-light-coral">
-              Ask us about
-            </h2>
-            <div className="flex flex-col gap-5">
-              <ContactItem
-                icon="/assets/icon-person.svg"
-                label="The quality of our talent network"
-              />
-              <ContactItem
-                icon="/assets/icon-cog.svg"
-                label="Usage & implementation of our software"
-              />
-              <ContactItem
-                icon="/assets/icon-chart.svg"
-                label="How we help drive innovation"
-              />
+          <div className="flex flex-col lg:flex-row lg:justify-center">
+            <div className="basis-1/2 flex flex-col items-center gap-5 px-5 mb-10">
+              <h1 className="text-4xl font-bold text-white">Contact</h1>
+              <h2 className="text-3xl font-bold text-light-coral">
+                Ask us about
+              </h2>
+              <div className="flex flex-col gap-5">
+                <ContactItem
+                  icon="/assets/icon-person.svg"
+                  label="The quality of our talent network"
+                />
+                <ContactItem
+                  icon="/assets/icon-cog.svg"
+                  label="Usage & implementation of our software"
+                />
+                <ContactItem
+                  icon="/assets/icon-chart.svg"
+                  label="How we help drive innovation"
+                />
+              </div>
             </div>
+            <form action="" className="basis-1/2 px-5">
+              <div className="flex flex-col gap-5 mb-5">
+                <Input label="Name" />
+                <Input label="Email Address" />
+                <Input label="Company Name" />
+                <Input label="Title" />
+                <Input type={InputType.TextArea} label="Message" />
+              </div>
+              <Button
+                label="submit"
+                color={ButtonColor.secondary}
+                className="grow-0"
+              />
+            </form>
           </div>
-          <form action="" className="px-5">
-            <div className="flex flex-col gap-5 mb-5">
-              <Input label="Name" />
-              <Input label="Email Address" />
-              <Input label="Company Name" />
-              <Input label="Title" />
-              <Input type={InputType.TextArea} label="Message" />
-            </div>
-            <Button
-              label="submit"
-              color={ButtonColor.secondary}
-              className="grow-0"
-            />
-          </form>
         </Container>
       </section>
     </BaseScreen>
