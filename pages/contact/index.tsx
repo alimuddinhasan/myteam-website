27cdabfbeaf7d792745f135a991e8cea2cd4ba9b
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-09-01 05:28:46 +0700

    feat(contact): add button submit

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -2,6 +2,7 @@ import BaseScreen from "@/components/BaseScreen";
 import ContactItem from "@/components/ContactItem";
 import Container from "@/components/Container";
 import Input, { InputType } from "@/components/Input";
+import Button, { ButtonColor } from "@/components/Button";
 
 export default function Contact() {
   return (
@@ -9,12 +10,9 @@ export default function Contact() {
       pageTitle="Frontend Mentor | myteam website challenge - Contact"
       showGetStarted={false}
     >
-      <section
-        role="contact"
-        className="bg-midnight-green flex justify-center  "
-      >
-        <Container>
-          <div className="flex flex-col items-center gap-5 px-5 py-24">
+      <section role="contact" className="bg-midnight-green flex justify-center">
+        <Container className="py-24">
+          <div className="flex flex-col items-center gap-5 px-5 mb-10">
             <h1 className="text-4xl font-bold text-white">Contact</h1>
             <h2 className="text-3xl font-bold text-light-coral">
               Ask us about
@@ -34,13 +32,20 @@ export default function Contact() {
               />
             </div>
           </div>
-          <div className="flex flex-col items-stretch gap-5 px-5 py-24">
-            <Input label="Name" />
-            <Input label="Email Address" />
-            <Input label="Company Name" />
-            <Input label="Title" />
-            <Input type={InputType.TextArea} label="Message" />
-          </div>
+          <form action="" className="px-5">
+            <div className="flex flex-col gap-5 mb-5">
+              <Input label="Name" />
+              <Input label="Email Address" />
+              <Input label="Company Name" />
+              <Input label="Title" />
+              <Input type={InputType.TextArea} label="Message" />
+            </div>
+            <Button
+              label="submit"
+              color={ButtonColor.secondary}
+              className="grow-0"
+            />
+          </form>
         </Container>
       </section>
     </BaseScreen>
