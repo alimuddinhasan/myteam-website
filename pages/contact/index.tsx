f72944d5681c4794b412e3d508d38e58c020e935
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-09-01 05:20:09 +0700

    feat(contact): update fields

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,34 +1,45 @@
 import BaseScreen from "@/components/BaseScreen";
 import ContactItem from "@/components/ContactItem";
 import Container from "@/components/Container";
+import Input, { InputType } from "@/components/Input";
 
 export default function Contact() {
   return (
     <BaseScreen
-      pageTitle='Frontend Mentor | myteam website challenge - Contact'
+      pageTitle="Frontend Mentor | myteam website challenge - Contact"
       showGetStarted={false}
     >
-      <section role='contact' className='bg-midnight-green flex justify-center'>
+      <section
+        role="contact"
+        className="bg-midnight-green flex justify-center  "
+      >
         <Container>
-          <div className='flex flex-col items-center gap-5 px-5 py-24'>
-            <h1 className='text-4xl font-bold text-white'>Contact</h1>
-            <h2 className='text-3xl font-bold text-light-coral'>
+          <div className="flex flex-col items-center gap-5 px-5 py-24">
+            <h1 className="text-4xl font-bold text-white">Contact</h1>
+            <h2 className="text-3xl font-bold text-light-coral">
               Ask us about
             </h2>
-            <div className='flex flex-col gap-5'>
+            <div className="flex flex-col gap-5">
               <ContactItem
-                icon='/assets/icon-person.svg'
-                label='The quality of our talent network'
+                icon="/assets/icon-person.svg"
+                label="The quality of our talent network"
               />
               <ContactItem
-                icon='/assets/icon-cog.svg'
-                label='Usage & implementation of our software'
+                icon="/assets/icon-cog.svg"
+                label="Usage & implementation of our software"
               />
               <ContactItem
-                icon='/assets/icon-chart.svg'
-                label='How we help drive innovation'
+                icon="/assets/icon-chart.svg"
+                label="How we help drive innovation"
               />
             </div>
+          </div>
+          <div className="flex flex-col items-stretch gap-5 px-5 py-24">
+            <Input label="Name" />
+            <Input label="Email Address" />
+            <Input label="Company Name" />
+            <Input label="Title" />
+            <Input type={InputType.TextArea} label="Message" />
           </div>
         </Container>
       </section>
