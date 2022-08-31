4ec7fd4dd8bad98ecd69d53422c10c0529437420
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-09-01 06:07:38 +0700

    feat(home): add responsive to featured

--- components/FeaturedItem/index.tsx
+++ components/FeaturedItem/index.tsx
@@ -12,11 +12,13 @@ export default function FeaturedItem({
   title,
 }: IFeaturedItemProps) {
   return (
-    <div className='flex flex-col gap-3 items-center'>
-      <img src={icon} alt='featured-icon' />
-      <div className="flex flex-col items-center gap-3">
-        <h2 className='text-light-coral font-bold text-sm'>{title}</h2>
-        <p className='text-center text-white text-xs leading-6'>{description}</p>
+    <div className="flex flex-col lg:flex-row gap-3 items-center">
+      <img src={icon} alt="featured-icon" />
+      <div className="flex flex-col items-center lg:items-start gap-3">
+        <h2 className="text-light-coral font-bold text-sm">{title}</h2>
+        <p className="text-center lg:text-left text-white text-xs leading-6">
+          {description}
+        </p>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,45 +5,45 @@ import { Container, Header } from "../components";
 
 export default function Home() {
   return (
-    <BaseScreen pageTitle='Frontend Mentor | myteam website challenge - Homepage'>
+    <BaseScreen pageTitle="Frontend Mentor | myteam website challenge - Homepage">
       <Header />
-      <section className='bg-sacramento-state-green overflow-hidden '>
-        <img
-          src='/assets/bg-pattern-home-3.svg'
-          alt='bg-featured'
-          className='float-right h-[244px] w-[200px] translate-x-1/2'
-        />
+      <section className="bg-sacramento-state-green overflow-hidden flex justify-center">
+        {/*<img*/}
+        {/*  src="/assets/bg-pattern-home-3.svg"*/}
+        {/*  alt="bg-featured"*/}
+        {/*  className="float-right h-[244px] w-[200px] translate-x-1/2"*/}
+        {/*/>*/}
         <Container>
-          <div className='px-5 py-10'>
-            <div className='flex flex-col justify-center max-w-5xl gap-8'>
+          <div className="px-5 py-10 lg:py-24 flex flex-col lg:flex-row items-center gap-8 lg:items-start">
+            <div className="lg:basis-1/2 flex flex-col justify-center max-w-5xl gap-8">
               <hr
-                className='w-12 border-2 border-light-coral'
-                data-testid='h-line'
+                className="w-12 border-2 border-light-coral"
+                data-testid="h-line"
               />
-              <h1 className='text-2xl font-bold text-white'>
+              <h1 className="text-2xl lg:text-5xl font-bold text-white">
                 Build &amp; manage distributed teams like no one else.
               </h1>
             </div>
-            <div className='flex flex-col gap-8 mt-16'>
+            <div className="lg:basis-1/2 flex flex-col gap-8">
               <FeaturedItem
-                title='Experienced Individuals'
-                description='Our network is made up of highly experienced professionals who are passionate about what they do.'
-                icon='assets/icon-person.svg'
+                title="Experienced Individuals"
+                description="Our network is made up of highly experienced professionals who are passionate about what they do."
+                icon="assets/icon-person.svg"
               />
               <FeaturedItem
-                title='Experienced Individuals'
-                description='Our network is made up of highly experienced professionals who are passionate about what they do.'
-                icon='assets/icon-person.svg'
+                title="Experienced Individuals"
+                description="Our network is made up of highly experienced professionals who are passionate about what they do."
+                icon="assets/icon-person.svg"
               />
               <FeaturedItem
-                title='Experienced Individuals'
-                description='Our network is made up of highly experienced professionals who are passionate about what they do.'
-                icon='assets/icon-person.svg'
+                title="Experienced Individuals"
+                description="Our network is made up of highly experienced professionals who are passionate about what they do."
+                icon="assets/icon-person.svg"
               />
               <FeaturedItem
-                title='Experienced Individuals'
-                description='Our network is made up of highly experienced professionals who are passionate about what they do.'
-                icon='assets/icon-person.svg'
+                title="Experienced Individuals"
+                description="Our network is made up of highly experienced professionals who are passionate about what they do."
+                icon="assets/icon-person.svg"
               />
             </div>
           </div>
@@ -51,30 +51,30 @@ export default function Home() {
       </section>
       <section className='bg-deep-jungle-green py-36 px-5 bg-[url("/assets/bg-pattern-home-4-about-3.svg"),_url("/assets/bg-pattern-home-5.svg")] bg-no-repeat bg-[position:left_top,_right_bottom] flex justify-center'>
         <Container>
-          <div className='flex flex-col items-center gap-8'>
-            <h1 className='font-bold text-2xl lg:text-5xl text-center text-white'>
+          <div className="flex flex-col items-center gap-8">
+            <h1 className="font-bold text-2xl lg:text-5xl text-center text-white">
               Delivering real results for top companies. Some of our{" "}
-              <span className='text-rapture-blue'>success stories.</span>
+              <span className="text-rapture-blue">success stories.</span>
             </h1>
-            <div className='flex flex-col lg:flex-row items-center gap-8'>
+            <div className="flex flex-col lg:flex-row items-center gap-8">
               <Review
-                review='We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!'
-                name='Aiysha Reese'
-                jobTitle='Founder of Manage'
-                avatar='assets/avatar-kady.jpg'
+                review="We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!"
+                name="Aiysha Reese"
+                jobTitle="Founder of Manage"
+                avatar="assets/avatar-kady.jpg"
               />
 
               <Review
-                review='We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!'
-                name='Aiysha Reese'
-                jobTitle='Founder of Manage'
-                avatar='assets/avatar-kady.jpg'
+                review="We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!"
+                name="Aiysha Reese"
+                jobTitle="Founder of Manage"
+                avatar="assets/avatar-kady.jpg"
               />
               <Review
-                review='We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!'
-                name='Aiysha Reese'
-                jobTitle='Founder of Manage'
-                avatar='assets/avatar-kady.jpg'
+                review="We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!"
+                name="Aiysha Reese"
+                jobTitle="Founder of Manage"
+                avatar="assets/avatar-kady.jpg"
               />
             </div>
           </div>
