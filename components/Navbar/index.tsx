c3e1c4c2bc62be37da658c4308778beffe26dc77
Alimuddin Hasan <mt-alimuddin.hasanalkabir@tech.jago.com>
2022-09-07 23:26:34 +0700

    feat(navbar): add route

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,27 +1,42 @@
 import React from "react";
 import Button from "../Button";
 import Container from "../Container";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav
-      className='flex justify-center bg-midnight-green pt-10 px-5 relative'
-      role='navbar'
+      className="flex justify-center bg-midnight-green pt-10 px-5 relative"
+      role="navbar"
     >
       <Container>
-        <div className='flex flex-col items-center'>
-          <div className='flex justify-between items-center self-stretch'>
-            <div className='flex items-center gap-5'>
-              <img src='assets/logo.svg' className='mr-5' alt='myteam-logo' />
-              <Button className='hidden sm:inline-block' label='home' isFlat />
-              <Button className='hidden sm:inline-block' label='about' isFlat />
+        <div className="flex flex-col items-center">
+          <div className="flex justify-between items-center self-stretch">
+            <div className="flex items-center gap-5">
+              <img src="/assets/logo.svg" className="mr-5" alt="myteam-logo" />
+              <Link href="/" passHref>
+                <Button
+                  className="hidden sm:inline-block"
+                  label="home"
+                  isFlat
+                />
+              </Link>
+              <Link href="/about" passHref>
+                <Button
+                  className="hidden sm:inline-block"
+                  label="about"
+                  isFlat
+                />
+              </Link>
             </div>
             <Button
-              className='sm:hidden'
-              icon='assets/icon-hamburger.svg'
+              className="sm:hidden"
+              icon="assets/icon-hamburger.svg"
               isFlat
             />
-            <Button className='hidden sm:inline-block' label='contact us' />
+            <Link href="/contact" passHref>
+              <Button className="hidden sm:inline-block" label="contact us" />
+            </Link>
           </div>
         </div>
       </Container>
